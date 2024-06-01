f0ddec87c45ae9e4a019b99ad7e12e9c3566cba3
uobiel <gabrielantunesabc@gmail.com>
2024-05-31 22:30:55 -0300

    Adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
-import { SessaoUmComponent } from './components/sessao-um/sessao-um.component';
-import { SessaoDoisComponent } from './components/sessao-dois/sessao-dois.component';
-import { SessaoTresComponent } from './components/sessao-tres/sessao-tres.component';
-import { SessaoQuatroComponent } from './components/sessao-quatro/sessao-quatro.component';
-import { SessaoCincoComponent } from './components/sessao-cinco/sessao-cinco.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabecalhoComponent, 
-            SessaoUmComponent, SessaoDoisComponent, 
-            SessaoTresComponent, SessaoQuatroComponent,
-            SessaoCincoComponent],
+  imports: [RouterOutlet, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
